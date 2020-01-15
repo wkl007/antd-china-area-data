80e9a3a183a68a306745c6320af3508c51e545d0
wkl007 <499657357@qq.com>
2020-01-15 10:20:07 +0000

    Deploying to gh-pages from example 4dfc90107cfbe7bacbbcc267cd9581a5a56f7b7b

---
+++ precache-manifest.933b0b20722bfdec1c37061d55e0b239.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a5c1455b28fbd52bb6ec548bb4422f",
+    "url": "/antd-china-area-data/index.html"
+  },
+  {
+    "revision": "c462dbff1b190bf68567",
+    "url": "/antd-china-area-data/static/css/2.3e9ce9d5.chunk.css"
+  },
+  {
+    "revision": "f25de3fff62e9a7ba6b8",
+    "url": "/antd-china-area-data/static/css/main.c1bb0f10.chunk.css"
+  },
+  {
+    "revision": "c462dbff1b190bf68567",
+    "url": "/antd-china-area-data/static/js/2.0e10fac3.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/antd-china-area-data/static/js/2.0e10fac3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f25de3fff62e9a7ba6b8",
+    "url": "/antd-china-area-data/static/js/main.1a424c49.chunk.js"
+  },
+  {
+    "revision": "cb3ff1956268382f8c5d",
+    "url": "/antd-china-area-data/static/js/runtime-main.22a4d2b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/antd-china-area-data/static/media/logo.5d5d9eef.svg"
+  }
+]);
